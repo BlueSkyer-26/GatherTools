7b11b082745e0f25c045d4894b4e3fc695652967
sunboy贾 <dongliang.jia@txhpro.com>
2017-02-14 11:34:23 +0800

    first。

---
+++ GatherTools/GatherTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GatherTools
+//
+//  Created by SunBoy on 17/2/14.
+//  Copyright © 2017年 JDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GatherTools/GatherTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GatherTools
+//
+//  Created by SunBoy on 17/2/14.
+//  Copyright © 2017年 JDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
