1fe05be8e9e4e496159d0972f6fb37035bccc52a
sunboy贾 <dongliang.jia@txhpro.com>
2017-02-28 11:27:55 +0800

    我的页面tableview。

--- GatherTools/GatherTools/Main/Mine/Controller/GTMineViewController.m
+++ GatherTools/GatherTools/Main/Mine/Controller/GTMineViewController.m
@@ -8,16 +8,51 @@
 
 #import "GTMineViewController.h"
 
-@interface GTMineViewController ()
+@interface GTMineViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property(nonatomic,strong)UITableView *tableView;
+@property(nonatomic,strong)NSArray *items;
 
 @end
 
 @implementation GTMineViewController
 
+-(UITableView *)tableView{
+    if(!_tableView){
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, Main_Screen_Width, Main_Screen_Height) style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.navigationItem.title = @"tools";
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.tableView.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:_tableView];
+    
+    self.items = @[@"二维码扫描",
+                    @"第三方分享",
+                    @"自定义弹窗",
+                    @"网络连接失败页面/无数据界面",
+                    @"加载数据是显示占位页面",
+                    @"搜索界面"];
     // Do any additional setup after loading the view.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.items.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString * indentifier = @"cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:indentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:indentifier];
+    }
+    cell.textLabel.text = self.items[indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
